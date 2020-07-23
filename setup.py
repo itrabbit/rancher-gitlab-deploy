93a5dcc435127de784cfb25efceabaa1aad23012
Alexander Garin <garin1221@yandex.ru>
2020-07-23 15:20:01 +0300

    Update setup.py
    1.5.2

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(name='rancher-gitlab-deploy',
-    version='1.5',
+    version='1.5.2',
     description='Command line tool to ease updating services in Rancher from your GitLab CI pipeline',
-    url='https://github.com/cdrx/rancher-gitlab-deploy',
-    author='cdrx',
+    url='https://github.com/itrabbit/rancher-gitlab-deploy',
+    author='cdrx, modifier - itrabbit',
     license='MIT',
     packages=['rancher_gitlab_deploy'],
     zip_safe=False,
